525fc99d04777d08e1cee1a34ee12ddc285b660d
Kevin Strom <kevinastrom@me.com>
2024-12-31 17:39:15 -0800

    Updated library example sketch.

--- examples/A0221AT_example.cpp
+++ examples/A0221AT_example.cpp
@@ -1,30 +1,22 @@
 #include <Arduino.h>
 #include <A0221AT.h>
 
-// Define the UART pins (use different pins if necessary)
-#define SENSOR_RX_PIN 0  // Nano RP2040 Connect RX1 (GPIO0)
-#define SENSOR_TX_PIN 1  // Nano RP2040 Connect TX1 (GPIO1)
-
-// Create a HardwareSerial instance (Serial1 is already defined for pins 0 & 1)
-A0221AT ultrasonicSensor(Serial1, SENSOR_RX_PIN, SENSOR_TX_PIN);
+// Create an A0221AT object using Serial1 (on pins 0,1 by default)
+A0221AT ultrasonicSensor(Serial1);
 
 void setup() {
   // Initialize the sensor
+  // - 9600 for sensor UART
+  // - 115200 for USB monitor
   ultrasonicSensor.begin(9600, 115200);
-
-  Serial.println("A0221AT Ultrasonic Sensor Example");
 }
 
 void loop() {
   float distance_cm;
-
-  // Get distance measurement
   if (ultrasonicSensor.getDistance(distance_cm)) {
     Serial.print("Distance: ");
     Serial.print(distance_cm);
     Serial.println(" cm");
   }
-
-  // Wait before next measurement
   delay(1000);
 }
